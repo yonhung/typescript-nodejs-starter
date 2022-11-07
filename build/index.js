b0f463585c93e98536c7f2b87d41e14a2ddafde0
Starbugz <wuyonghang12138@gmail.com>
2022-11-07 15:32:32 +0800

    initial

---
+++ build/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const jack = {
+    name: "Jack",
+    id: 1
+};
+console.log(jack);
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+type User = {
+  name: string;
+  id: number;
+}
+
+const jack: User = {
+  name: "Jack",
+  id: 1
+}
+
+console.log(jack);
